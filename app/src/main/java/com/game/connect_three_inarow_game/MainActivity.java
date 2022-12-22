a53ae007df93dd7c43651b88f21347783d58bc34
idanr@playtika.com <idanr@playtika.com>
2022-12-22 21:05:53 +0200

    WIP

--- app/src/main/java/com/game/connect_three_inarow_game/MainActivity.java
+++ app/src/main/java/com/game/connect_three_inarow_game/MainActivity.java
@@ -3,12 +3,31 @@ package com.game.connect_three_inarow_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).setDuration(300);
+
+        Log.i("Info", "The image has been clicked");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
